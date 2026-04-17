9e028ab2040f85c6a08354e6d6715f0c022d8935
Fatima Arain <fatimaarain67@gmail.com>
2026-04-17 17:02:59 +0500

    creating floders and files

---
+++ src/test/java/org/example/smartdiet/SmartDietApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartdiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDietApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
